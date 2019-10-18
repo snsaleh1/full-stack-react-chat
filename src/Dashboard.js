5a55c90710f503f4e2bf27ad5d6631bbde081679
snsaleh1 <snsaleh11@gmail.com>
2019-10-18 11:28:40 -0500

    dependencies installed dashboard rendered

---
+++ src/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(3, 2),
+    },
+  }));
+
+export default function Dashboard() {
+
+    const classes = useStyles();
+  return (
+      <div>
+        <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build POPO or other elements for your PIPO.
+        </Typography>
+      </Paper>
+      </div>
+  )  
+}
