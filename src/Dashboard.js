42d3e1eab3a0fa1ba232c370472372ccf77f4b25
snsaleh1 <snsaleh11@gmail.com>
2019-10-18 16:17:02 -0500

    user interface set

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,25 +2,91 @@ import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
 
 const useStyles = makeStyles(theme => ({
     root: {
+        margin: '50px',
       padding: theme.spacing(3, 2),
+    },
+    flex: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    topicsWindow: {
+        width: '30%',
+        height: '300px',
+        borderRight: '1px solid grey'
+    },
+    chatWindow: {
+        width: '70%',
+        height: '300px',
+        padding: '20px'
+    },
+    chatBox: {
+        width: '85%'
+    },
+    button: {
+        width: '15%'
     },
   }));
 
 export default function Dashboard() {
 
     const classes = useStyles();
+
+    const [textValue, changeTextValue] = React.useState('');
+
   return (
       <div>
         <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          This is a sheet of paper.
+        <Typography variant="h4" component="h4">
+            Caleb's Chat App
         </Typography>
-        <Typography component="p">
-          Paper can be used to build POPO or other elements for your PIPO.
+        <Typography variant="h5" component="h5">
+          Topic Placeholder
         </Typography>
+        <div className={classes.flex}>
+            <div className={classes.topicsWindow}>
+                <List>
+                    {
+                        ['topic'].map(topic => (
+                            <ListItem key={topic} button>
+                                <ListItemText primary="topic" />
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </div>
+            <div className={classes.chatWindow}>
+                {
+                    [{from: 'user', msg:'hello'}].map((chat, i) => (
+                        <div className={classes.flex} key={i}>
+                            <Chip label={chat.from} className={classes.chip} />
+                            <Typography variant='p'>{chat.msg}</Typography>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+        <div className={classes.flex}>
+            <TextField
+                label="Send a Message"
+                className={classes.chatBox}
+                value={textValue}
+                onChange={e => changeTextValue(e.target.value)}
+            />
+            <Button variant="contained" color="primary" className={classes.button}>
+                Send
+            </Button>
+        </div>
       </Paper>
       </div>
   )  
